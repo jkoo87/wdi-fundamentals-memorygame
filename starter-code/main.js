261c1a6bacfb9ca1b0358e48473504eaf8a18d16
Jeong Hoon Koo <junghoone17@gmail.com>
2017-03-20 00:30:53 -0400

    adding function

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,12 +1,22 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "king"
-var cardTwo = "queen"
-var cardThree = "queen"
-var cardFour = "king"
+var cardOne = 'king'
+var cardTwo = 'queen'
+var cardThree = 'queen'
+var cardFour = 'king'
+
 if (cardOne === cardFour) {
-	alert("You found a match!");
+	alert('You found a match!');
 } else if (cardTwo === cardThree) {
-	alert("You found a match!");
+	alert('You found a match!');
 } else {
-	alert("Sorry, try again.");
+	alert('Sorry, try again.');
+}
+
+var board = document.getElementById('game-board');
+function createCards () {
+	for (var i=0; i<cards.length; 1++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		board.appendChild(cardElement);
+	}
 }
