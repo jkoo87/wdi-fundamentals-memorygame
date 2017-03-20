152a8fd8ef984db2751fc730b8f8cbd145e59dc7
Jeong Hoon Koo <junghoone17@gmail.com>
2017-03-20 03:15:47 -0400

    complete

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,22 +1,39 @@
-console.log("JS file is connected to HTML! Woo!")
-var cardOne = 'king'
-var cardTwo = 'queen'
-var cardThree = 'queen'
-var cardFour = 'king'
 
-if (cardOne === cardFour) {
-	alert('You found a match!');
-} else if (cardTwo === cardThree) {
-	alert('You found a match!');
-} else {
-	alert('Sorry, try again.');
-}
+var cards = ['king', 'queen', 'queen', 'king'];
+var cardsInPlay =[];
+
 
 var board = document.getElementById('game-board');
-function createCards () {
-	for (var i=0; i<cards.length; 1++) {
+function createBoard() {
+	for (var i=0; i<cards.length; i++) {
 		var cardElement = document.createElement('div');
 		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+		board.appendChild(cardElement);
 		board.appendChild(cardElement);
 	}
 }
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+console.log(this.getAttribute('data-card'));
+if (this.getAttribute('data-card') === 'king') {
+this.innerHTML = "<img src='http://i.imgur.com/bnuv5Im.png'>"; // king
+} else {
+this.innerHTML = "<img src='http://i.imgur.com/v6buNt2.png'>"; //queen
+} if (cardsInPlay.length === 2) {
+	isMatch(cardsInPlay);
+	cardsInPlay = [];
+  }
+}
+
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
+
+createBoard();
